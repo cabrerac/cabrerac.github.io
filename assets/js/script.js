0ebabe1eeb08fb82426e526c80cba0686ccc9d6f
cabrerac <chc79@cam.ac.uk>
2023-03-28 00:09:11 +0100

    adding script to head

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const menuButton = document.getElementById('toggle-menu');
-const menu = document.querySelector('nav ul');
+const menu = document.querySelector('nav');
 
 menuButton.addEventListener('click', () => {
   menu.classList.toggle('menu-open');
