09c885ca98deb13013e9805dc88725e1075dbfdf
cabrerac <chc79@cam.ac.uk>
2023-05-01 16:02:04 +0100

    adding social network links

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menuButton = document.getElementById('toggle-menu');
-  const menu = document.querySelector('nav');
-
-  menuButton.addEventListener('click', () => {
-    menu.classList.toggle('menu-open');
-  });
-});
