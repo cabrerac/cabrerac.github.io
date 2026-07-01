fb66fbf848eeced0e386d79780ae0ce50f735508
cabrerac <christiancabrera89@gmail.com>
2026-07-01 20:02:16 +0100

    improving style

--- assets/js/collapse.js
+++ assets/js/collapse.js
@@ -1,21 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll('.collapsible-header').forEach(header => {
-    header.addEventListener('click', () => {
+  document.querySelectorAll(".collapsible-header").forEach((header) => {
+    header.addEventListener("click", () => {
       const content = header.nextElementSibling;
-      const isActive = header.classList.contains('active');
-
-      if (!isActive) {
-        document.querySelectorAll('.collapsible-header').forEach(otherHeader => {
-          if (otherHeader !== header) {
-            const otherContent = otherHeader.nextElementSibling;
-            otherHeader.classList.remove('active');
-            otherContent.classList.remove('expanded');
-          }
-        });
+      if (!content || !content.classList.contains("collapsible-content")) {
+        return;
       }
-
-      header.classList.toggle('active');
-      content.classList.toggle('expanded');
+      header.classList.toggle("active");
+      content.classList.toggle("expanded");
     });
   });
 });
